a73c0fafb7ca29b9b240425b90d7c46777e0f7c7
Jeovan <jeovandeabreu09@gmail.com>
2025-04-05 10:05:12 -0300

    Primeiro commit

---
+++ 002.py
@@ -0,0 +1,9 @@
+# Maioridade penal
+
+nome = input('informe o nome:')
+idade = int(input('informe a idade:'))
+
+if idade < 18:
+    print(nome, "Nao possui maioridade penal")
+else:
+    print(nome,"Ja possui maioridade penal")
