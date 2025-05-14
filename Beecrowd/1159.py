14228f7f08230876bd1739f715e0a673c92893d0
Jeovan <jeovandeabreu09@gmail.com>
2025-05-14 12:17:11 -0300

    commit

--- Beecrowd/1159.py
+++ Beecrowd/1159.py
@@ -1,13 +1,2 @@
-soma = 0
-
-while True:
-    n = int(input())
-    if n == 0:
-        break
-
-    if n % 2 == 0:
-        soma += n + n+2 + n+4 + n+6 + n+8
-    elif n % 2 != 0:
-        soma += n+1 + n+3 + n+5 + n+7 + n+9
-    print(soma)
-    soma = 0
+for i in range(2,101,2):
+    print(i)
